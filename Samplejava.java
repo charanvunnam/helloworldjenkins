dce9a536f862f31f2b4958293724808758a7b2b3
charanvunnam <69756990+charanvunnam@users.noreply.github.com>
2020-09-29 16:56:59 +0530

    Update Samplejava.java

--- Samplejava.java
+++ Samplejava.java
@@ -2,6 +2,6 @@ class Samplejava
 {
 public static void main (String args[])
 {
-System.out.println("Hello jenkins is running from github");
+System.out.println("Hello jenkins is running from webhook");
 }
 }
